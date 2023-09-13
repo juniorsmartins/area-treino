164f642d66198e069f3f67207b4a31da84ffd9fd
juniorsmartins <juniorsoaresmartins@gmail.com>
2023-09-13 19:16:11 -0400

    REFACTOR: reconstrução do projeto.

---
+++ alura-games/src/main/kotlin/io/aluragames/AluraGamesApplication.kt
@@ -0,0 +1,26 @@
+package io.aluragames
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse.BodyHandlers
+
+@SpringBootApplication
+class AluraGamesApplication
+
+fun main(args: Array<String>) {
+	runApplication<AluraGamesApplication>(*args)
+
+	val client: HttpClient = HttpClient.newHttpClient()
+	val request = HttpRequest.newBuilder()
+		.uri(URI.create("https://www.cheapshark.com/api/1.0/games?id=146"))
+		.build()
+	val response = client
+		.send(request, BodyHandlers.ofString())
+
+	val json = response.body()
+	println(json)
+}
