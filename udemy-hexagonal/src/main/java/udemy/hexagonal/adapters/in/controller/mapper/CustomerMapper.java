3867a176955f1b48ca9a36175e030d6370f734e5
juniorsmartins <juniorsoaresmartins@gmail.com>
2023-09-14 15:41:13 -0400

    FEAT: add controller, mapper, request e inputport.

---
+++ udemy-hexagonal/src/main/java/udemy/hexagonal/adapters/in/controller/mapper/CustomerMapper.java
@@ -0,0 +1,16 @@
+package udemy.hexagonal.adapters.in.controller.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import udemy.hexagonal.adapters.in.controller.request.CustomerRequest;
+import udemy.hexagonal.application.core.domain.Customer;
+
+@Mapper(componentModel = "spring")
+public interface CustomerMapper {
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "address", ignore = true)
+    @Mapping(target = "isValidCpf", ignore = true)
+    Customer toCustomer(CustomerRequest customerRequest);
+}
+
