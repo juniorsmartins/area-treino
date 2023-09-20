1733c69888c4f00f65a7315514be787b5f1b2c5e
juniorsmartins <juniorsoaresmartins@gmail.com>
2023-09-20 11:38:11 -0400

    ...

--- udemy-apirest-java/src/main/java/io/udemyapirestjava/MathController.java
+++ udemy-apirest-java/src/main/java/io/udemyapirestjava/MathController.java
@@ -10,8 +10,26 @@ public class MathController {
 
     @RequestMapping(path = "/sum/{numberOne}/{numberTwo}", method = RequestMethod.GET)
     public Double sum(@PathVariable(name = "numberOne") String numberOne,
-                      @PathVariable(name = "numberTwo") String numberTwo) {
-        return 1D;
+                      @PathVariable(name = "numberTwo") String numberTwo) throws Exception {
+
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+            throw new Exception();
+        }
+
+        return convertToDouble(numberOne) + convertToDouble(numberTwo);
+    }
+
+    private Double convertToDouble(String strNumber) {
+        if (strNumber == null) return 0D;
+        String number = strNumber.replaceAll(",", ".");
+        if (isNumeric(number)) return Double.parseDouble(number);
+        return 0D;
+    }
+
+    private boolean isNumeric(String strNumber) {
+        if (strNumber == null) return false;
+        String number = strNumber.replaceAll(",", ".");
+        return number.matches("[-+]?[0-9]*\\.?[0-9]+");
     }
 }
 
