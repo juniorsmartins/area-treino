4befbebb9be3aa79c1ff6aa089cbffbbbc9d9e88
juniorsmartins <juniorsoaresmartins@gmail.com>
2023-11-02 16:25:09 -0400

    FEAT: add Security

--- api-rest.estacionamento.java/src/main/java/io/apirest/estacionamento/java/security/dto/UsuarioLoginDto.java
+++ api-rest.estacionamento.java/src/main/java/io/apirest/estacionamento/java/security/dto/UsuarioLoginDto.java
@@ -3,7 +3,15 @@ package io.apirest.estacionamento.java.security.dto;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class UsuarioLoginDto {
 
     @NotBlank
