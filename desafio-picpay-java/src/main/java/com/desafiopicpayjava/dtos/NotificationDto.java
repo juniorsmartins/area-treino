4b0956091b3bf73e6614ade11dadbcbfbc041ae4
juniorsmartins <juniorsoaresmartins@gmail.com>
2023-11-09 09:51:29 -0400

    FEAT: add NotificationService and Config RestTemplate.

---
+++ desafio-picpay-java/src/main/java/com/desafiopicpayjava/dtos/NotificationDto.java
@@ -0,0 +1,9 @@
+package com.desafiopicpayjava.dtos;
+
+public record NotificationDto(
+
+    String email,
+
+    String message
+) { }
+
---
+++ desafio-picpay-java/src/main/java/com/desafiopicpayjava/service/NotificationService.java
@@ -0,0 +1,31 @@
+package com.desafiopicpayjava.service;
+
+import com.desafiopicpayjava.domain.user.User;
+import com.desafiopicpayjava.dtos.NotificationDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class NotificationService {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public void sendNotification(User user, String message) throws Exception {
+
+        var email = user.getEmail();
+        var notificationRequest = new NotificationDto(email, message);
+
+        ResponseEntity<String> notificationResponse = this.restTemplate.postForEntity(
+            "http://o4d9z.mocklab.io/notify", notificationRequest, String.class);
+
+        if (!(notificationResponse.getStatusCode() == HttpStatus.OK)) {
+            System.out.println("Erro ao enviar notificação.");
+            throw new Exception("Serviço de notificação está fora do ar.");
+        }
+    }
+}
+
