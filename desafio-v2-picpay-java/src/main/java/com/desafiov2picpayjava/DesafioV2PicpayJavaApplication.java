8a610516e27c3dc13b808d9285283580673bf807
juniorsmartins <juniorsoaresmartins@gmail.com>
2023-11-09 13:25:55 -0400

    FEAT: first commit

---
+++ desafio-v2-picpay-java/src/main/java/com/desafiov2picpayjava/DesafioV2PicpayJavaApplication.java
@@ -0,0 +1,13 @@
+package com.desafiov2picpayjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioV2PicpayJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioV2PicpayJavaApplication.class, args);
+	}
+
+}
