14d6a259c7cb1c1fc434afe583cbdd1c55954404
juniorsmartins <juniorsoaresmartins@gmail.com>
2023-11-09 19:50:51 -0400

    FEAT: add Ports.

---
+++ desafio-v2-picpay-java/src/main/java/com/desafiov2picpayjava/application/ports/out/UsuarioSalvarOutputPort.java
@@ -0,0 +1,9 @@
+package com.desafiov2picpayjava.application.ports.out;
+
+import com.desafiov2picpayjava.application.core.domain.Usuario;
+
+public interface UsuarioSalvarOutputPort {
+
+    Usuario salvar(Usuario usuario);
+}
+
