3b79df0c39d90ee6faafea7e6e8de75cbac9f680
juniorsmartins <juniorsoaresmartins@gmail.com>
2023-11-09 10:21:17 -0400

    REFACTOR: correções.

--- desafio-picpay-java/src/main/java/com/desafiopicpayjava/domain/user/User.java
+++ desafio-picpay-java/src/main/java/com/desafiopicpayjava/domain/user/User.java
@@ -2,10 +2,7 @@ package com.desafiopicpayjava.domain.user;
 
 import com.desafiopicpayjava.dtos.UserDto;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.math.BigDecimal;
 
@@ -14,6 +11,7 @@ import java.math.BigDecimal;
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 @EqualsAndHashCode(of = {"id"})
 public class User {
 
