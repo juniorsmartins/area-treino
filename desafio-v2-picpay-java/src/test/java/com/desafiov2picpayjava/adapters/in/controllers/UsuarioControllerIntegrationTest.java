b8aecfce60e2ddde2fbffc9223c0f2690d5b9191
juniorsmartins <juniorsoaresmartins@gmail.com>
2023-11-09 19:48:45 -0400

    TEST: TDD do recurso Cadastrar Usuário.

---
+++ desafio-v2-picpay-java/src/test/java/com/desafiov2picpayjava/adapters/in/controllers/UsuarioControllerIntegrationTest.java
@@ -0,0 +1,48 @@
+package com.desafiov2picpayjava.adapters.in.controllers;
+
+import com.desafiov2picpayjava.adapters.in.dtos.UsuarioDtoIn;
+import com.desafiov2picpayjava.adapters.in.dtos.UsuarioDtoOut;
+import com.desafiov2picpayjava.utils.CriadorDeBuilders;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class UsuarioControllerIntegrationTest {
+
+    private static final String CAMINHO = "/api/v1/usuarios";
+
+    @Autowired
+    WebTestClient webTestClient;
+
+    @Test
+    @Order(1)
+    public void cadastrarUsuario_ComDadosValidos_RetornarUsuarioDtoOutComHttpStatus201() {
+
+        var dtoIn = CriadorDeBuilders.gerarUsuarioDtoInBuilder().build();
+
+        var resposta = this.webTestClient.post()
+            .uri(CAMINHO)
+            .contentType(MediaType.APPLICATION_JSON)
+            .bodyValue(dtoIn)
+            .exchange()
+            .expectStatus().isCreated()
+            .expectBody(UsuarioDtoOut.class)
+            .returnResult().getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(resposta).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(resposta.nome()).isEqualTo(dtoIn.nome());
+        org.assertj.core.api.Assertions.assertThat(resposta.documento()).isEqualTo(dtoIn.documento());
+        org.assertj.core.api.Assertions.assertThat(resposta.email()).isEqualTo(dtoIn.email());
+        org.assertj.core.api.Assertions.assertThat(resposta.senha()).isEqualTo(dtoIn.senha());
+        org.assertj.core.api.Assertions.assertThat(resposta.tipo()).isEqualToIgnoringCase(dtoIn.tipo());
+    }
+}
