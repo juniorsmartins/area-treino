4a4c4d5c8fcdb699109b6c0f4c52b4e499e07bf9
juniorsmartins <juniorsoaresmartins@gmail.com>
2023-11-10 11:54:59 -0400

    TEST: add scripts de SQL para testes.

---
+++ desafio-v2-picpay-java/src/test/resources/sql/usuarios-insert.sql
@@ -0,0 +1,5 @@
+INSERT INTO usuarios(version, id, nome, documento, email, senha, tipo) VALUES
+    (0, 10, 'Jeff Sutherland', '73249716014', 'jeff@email.com', '123456', 'LOJISTA'),
+    (0, 11, 'Kent Beck', '12319638083', 'kent@email.com', '123456', 'COMUM'),
+    (0, 12, 'Martin Fowler', '66675254030', 'fowler@email.com', '123456', 'COMUM');
+
