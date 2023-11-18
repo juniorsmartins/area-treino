aef39def2aca4a4938a8f7c1d70cc01dcb95d7f1
juniorsmartins <juniorsoaresmartins@gmail.com>
2023-11-18 15:48:13 -0400

    FEAT: add entities

---
+++ algaworks-algafood-java/src/main/java/io/algaworksalgafoodjava/domain/model/Estado.java
@@ -0,0 +1,30 @@
+package io.algaworksalgafoodjava.domain.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.io.Serial;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "estados")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode(of = {"id"})
+public final class Estado implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome", length = 50, nullable = false)
+    private String nome;
+}
+
