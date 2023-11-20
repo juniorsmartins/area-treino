5096848fee521597fbaf8bebb38debdc63757bf3
juniorsmartins <juniorsoaresmartins@gmail.com>
2023-11-20 13:49:03 -0400

    FEAT: add customização de retorno em XML de Cozinha

--- algaworks-algafood-java/src/main/java/io/algaworksalgafoodjava/api/controller/CozinhaController.java
+++ algaworks-algafood-java/src/main/java/io/algaworksalgafoodjava/api/controller/CozinhaController.java
@@ -1,5 +1,6 @@
 package io.algaworksalgafoodjava.api.controller;
 
+import io.algaworksalgafoodjava.api.wrapper.CozinhaXmlWrapper;
 import io.algaworksalgafoodjava.domain.model.Cozinha;
 import io.algaworksalgafoodjava.domain.repository.CozinhaRepository;
 import lombok.RequiredArgsConstructor;
@@ -12,15 +13,20 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "/api/v1/cozinhas", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+@RequestMapping(path = "/api/v1/cozinhas")
 @RequiredArgsConstructor
 public class CozinhaController {
 
     private final CozinhaRepository cozinhaRepository;
 
-    @GetMapping
+    @GetMapping(produces = {MediaType.APPLICATION_JSON_VALUE})
     public List<Cozinha> listar() {
         return this.cozinhaRepository.listar();
+    }
+
+    @GetMapping(produces = {MediaType.APPLICATION_XML_VALUE})
+    public CozinhaXmlWrapper listarXml() {
+        return new CozinhaXmlWrapper(this.cozinhaRepository.listar());
     }
 
     @GetMapping(path = "/{id}")
---
+++ algaworks-algafood-java/src/main/java/io/algaworksalgafoodjava/api/wrapper/CozinhaXmlWrapper.java
@@ -0,0 +1,21 @@
+package io.algaworksalgafoodjava.api.wrapper;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+import io.algaworksalgafoodjava.domain.model.Cozinha;
+import lombok.Data;
+import lombok.NonNull;
+
+import java.util.List;
+
+@JacksonXmlRootElement(localName = "cozinhas")
+@Data
+public class CozinhaXmlWrapper {
+
+    @JsonProperty("cozinha")
+    @JacksonXmlElementWrapper(useWrapping = false)
+    @NonNull
+    private List<Cozinha> cozinhas;
+}
+
