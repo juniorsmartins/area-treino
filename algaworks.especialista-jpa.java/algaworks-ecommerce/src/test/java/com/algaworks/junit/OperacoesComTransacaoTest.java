fffdbcc0c675b4d8399bded8f3970b8cb86c88e1
juniorsmartins <juniorsoaresmartins@gmail.com>
2023-11-23 16:33:31 -0400

    ...

--- algaworks.especialista-jpa.java/algaworks-ecommerce/src/test/java/com/algaworks/junit/OperacoesComTransacaoTest.java
+++ algaworks.especialista-jpa.java/algaworks-ecommerce/src/test/java/com/algaworks/junit/OperacoesComTransacaoTest.java
@@ -9,6 +9,99 @@ import java.math.BigDecimal;
 public class OperacoesComTransacaoTest extends EntityManagerTest {
 
     @Test
+    void mostrarDiferencaPersistAndMerge() {
+        var produtoPersist = Produto.builder()
+            .id(5)
+            .nome("Smartphone One Plus")
+            .descricao("O processador mais rápido.")
+            .preco(BigDecimal.valueOf(2450))
+            .build();
+
+        this.entityManager.getTransaction().begin();
+        this.entityManager.persist(produtoPersist);
+        produtoPersist.setNome("Smartphone Two Plus"); // Essa alteração é efetivada mesmo após a operação de persistir.
+        this.entityManager.getTransaction().commit();
+
+        this.entityManager.clear(); // Limpa a memória do EntityManager. Obrigará o find() abaixo a ir no banco buscar, ao invés de pegar na metória do EntityManager.
+
+        var produtoPraVerificar = this.entityManager.find(Produto.class, produtoPersist.getId());
+        Assertions.assertNotNull(produtoPraVerificar);
+
+        var produtoMerge = Produto.builder()
+            .id(6)
+            .nome("Notebook Dell XPS-9320")
+            .descricao("O processador mais rápido.")
+            .preco(BigDecimal.valueOf(11450))
+            .build();
+
+        this.entityManager.getTransaction().begin();
+        produtoMerge = this.entityManager.merge(produtoMerge); // É preciso capturar o retorno do Merge se quiser fazer alteração posterior.
+        produtoMerge.setDescricao("32 de Ram e o processador mais rápido.");
+        this.entityManager.getTransaction().commit();
+
+        this.entityManager.clear(); // Limpa a memória do EntityManager. Obrigará o find() abaixo a ir no banco buscar, ao invés de pegar na metória do EntityManager.
+
+        var produtoPraVerificar2 = this.entityManager.find(Produto.class, produtoMerge.getId());
+        Assertions.assertNotNull(produtoPraVerificar2);
+    }
+
+    @Test
+    void inserirObjetoComMerge() {
+        var produto = Produto.builder()
+            .id(4)
+            .nome("Microfone Sony")
+            .descricao("A melhor qualidade de som.")
+            .preco(BigDecimal.valueOf(1050))
+            .build();
+
+        this.entityManager.getTransaction().begin();
+        this.entityManager.merge(produto);
+        this.entityManager.getTransaction().commit();
+
+        this.entityManager.clear(); // Limpa a memória do EntityManager. Obrigará o find() abaixo a ir no banco buscar, ao invés de pegar na metória do EntityManager.
+
+        var produtoPraVerificar = this.entityManager.find(Produto.class, produto.getId());
+        Assertions.assertNotNull(produtoPraVerificar);
+        Assertions.assertEquals(produto.getNome(), produtoPraVerificar.getNome());
+    }
+
+    @Test
+    void atualizarObjetoGerenciado() {
+        var produto = this.entityManager.find(Produto.class, 1);
+
+        this.entityManager.getTransaction().begin();
+        produto.setNome("Kindle Paperwhite Geração 2");
+        this.entityManager.getTransaction().commit();
+
+        this.entityManager.clear();
+
+        var produtoPraVerificar = this.entityManager.find(Produto.class, produto.getId());
+        Assertions.assertEquals(produto.getNome(), produtoPraVerificar.getNome());
+    }
+
+    @Test
+    void atualizarObjeto() {
+        var produto = Produto.builder()
+            .id(1)
+            .nome("Kindle Paperwhite")
+            .descricao("Novo Kindle")
+            .preco(BigDecimal.valueOf(591.67))
+            .build();
+
+        this.entityManager.getTransaction().begin();
+        this.entityManager.merge(produto); // Se algum campo do produto estiver nulo, esse valor nulo substituirá o valor que estiver no banco
+        this.entityManager.getTransaction().commit();
+
+        this.entityManager.clear();
+
+        var produtoPraVerificar = this.entityManager.find(Produto.class, produto.getId());
+        Assertions.assertNotNull(produtoPraVerificar);
+        Assertions.assertEquals(produto.getNome(), produtoPraVerificar.getNome());
+        Assertions.assertEquals(produto.getDescricao(), produtoPraVerificar.getDescricao());
+        Assertions.assertEquals(produto.getPreco(), produtoPraVerificar.getPreco());
+    }
+
+    @Test
     void removerObjeto() {
         var produto = this.entityManager.find(Produto.class, 3);
 
