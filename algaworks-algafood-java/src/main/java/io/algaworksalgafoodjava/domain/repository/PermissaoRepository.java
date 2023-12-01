bbc95d4709eb7be9a39ffbdcfb98abddee9f5bf1
juniorsmartins <juniorsoaresmartins@gmail.com>
2023-12-01 17:53:20 -0400

    ...

--- algaworks-algafood-java/src/main/java/io/algaworksalgafoodjava/domain/repository/PermissaoRepository.java
+++ algaworks-algafood-java/src/main/java/io/algaworksalgafoodjava/domain/repository/PermissaoRepository.java
@@ -1,17 +1,9 @@
 package io.algaworksalgafoodjava.domain.repository;
 
 import io.algaworksalgafoodjava.domain.model.Permissao;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
-public interface PermissaoRepository {
-
-    List<Permissao> listar();
-
-    Permissao buscar(Long id);
-
-    Permissao salvar(Permissao permissao);
-
-    void remover(Permissao permissao);
-}
+@Repository
+public interface PermissaoRepository extends JpaRepository<Permissao, Long> { }
 
