5752c2fe541b15e85bfcb5e82025941eff182b27
juniorsmartins <juniorsoaresmartins@gmail.com>
2023-12-04 15:12:44 -0400

    FEAT: add repositório base customizado

--- algaworks-algafood-java/src/main/java/io/algaworksalgafoodjava/domain/repository/RestauranteRepository.java
+++ algaworks-algafood-java/src/main/java/io/algaworksalgafoodjava/domain/repository/RestauranteRepository.java
@@ -1,7 +1,6 @@
 package io.algaworksalgafoodjava.domain.repository;
 
 import io.algaworksalgafoodjava.domain.model.Restaurante;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
--- algaworks-algafood-java/src/main/java/io/algaworksalgafoodjava/infrastructure/repository/CustomJpaRepositoryImpl.java
+++ algaworks-algafood-java/src/main/java/io/algaworksalgafoodjava/infrastructure/repository/CustomJpaRepositoryImpl.java
@@ -9,17 +9,21 @@ import java.util.Optional;
 
 public class CustomJpaRepositoryImpl<T, I> extends SimpleJpaRepository<T, I> implements CustomJpaRepository<T, I> {
 
-    private EntityManager entityManager;
+    private final EntityManager entityManager;
 
     public CustomJpaRepositoryImpl(JpaEntityInformation<T, ?> entityInformation, EntityManager entityManager) {
         super(entityInformation, entityManager);
         this.entityManager = entityManager;
     }
+
     @Override
     public Optional<T> buscarPrimeiro() {
-        var jpql = "from " + super.getDomainClass().getName();
+        var tipoDaClasseT = super.getDomainClass();
+        var nomeDaClasseT = tipoDaClasseT.getName();
 
-        T entity = this.entityManager.createQuery(jpql, super.getDomainClass())
+        var jpql = "from " + nomeDaClasseT;
+
+        T entity = this.entityManager.createQuery(jpql, tipoDaClasseT)
             .setMaxResults(1)
             .getSingleResult();
 
