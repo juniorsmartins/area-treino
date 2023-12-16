c9a7cb7377e8f2fce30bac5c43e37d09d5cffb1d
juniorsmartins <juniorsoaresmartins@gmail.com>
2023-12-16 05:57:01 -0400

    TEST: aula sobre Assertions

--- curso-testes-unitarios-junit-projeto-main/src/test/java/com/algaworks/junit/utilidade/SaudacaoUtilTest.java
+++ curso-testes-unitarios-junit-projeto-main/src/test/java/com/algaworks/junit/utilidade/SaudacaoUtilTest.java
@@ -12,7 +12,20 @@ class SaudacaoUtilTest {
 
         String saudacao = SaudacaoUtil.saudar(9);
 
-        Assertions.assertTrue(saudacao.equals("Bom dia"));
+        Assertions.assertEquals("Bom dia", saudacao, "Saudação Incorreta!");
+    }
+
+    @Test
+    void deveLancarException() {
+
+        IllegalArgumentException excecao = assertThrows(IllegalArgumentException.class, () -> SaudacaoUtil.saudar(-10));
+        Assertions.assertEquals("Hora inválida", excecao.getMessage());
+    }
+
+    @Test
+    void naoDeveLancarExcecao() {
+
+        Assertions.assertDoesNotThrow(() -> SaudacaoUtil.saudar(0));
     }
 }
 
