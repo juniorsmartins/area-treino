e422fa3bc2fdfac4712c9a8f010d611dd9d4c383
juniorsmartins <juniorsoaresmartins@gmail.com>
2023-12-16 06:56:11 -0400

    TEST: como habilitar, desabilitar e atribuir condicional para execução de teste

--- curso-testes-unitarios-junit-projeto-main/src/test/java/com/algaworks/junit/utilidade/SaudacaoUtilTest.java
+++ curso-testes-unitarios-junit-projeto-main/src/test/java/com/algaworks/junit/utilidade/SaudacaoUtilTest.java
@@ -1,7 +1,10 @@
 package com.algaworks.junit.utilidade;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Assumptions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -24,6 +27,27 @@ class SaudacaoUtilTest {
 
     @Test
     void naoDeveLancarExcecao() {
+
+        Assertions.assertDoesNotThrow(() -> SaudacaoUtil.saudar(0));
+    }
+
+    @Test
+    @Disabled("forma de como desabilitar um teste")
+    void comoDesabilitarTeste() {
+
+        Assertions.assertDoesNotThrow(() -> SaudacaoUtil.saudar(0));
+    }
+
+    @Test
+    void comoDesabilitarPorCondicional() {
+
+        Assumptions.assumeTrue("PROD".equals(System.getenv("ENV")), () -> "Abortando Teste - não deve ser executado em PROD."); // Se a variável de ambiente for igual a PROD, não deve ser executado
+        Assertions.assertDoesNotThrow(() -> SaudacaoUtil.saudar(0));
+    }
+
+    @Test
+    @EnabledIfEnvironmentVariable(named = "ENV", matches = "PROD") // Se a variável de ambiente for igual a PROD, deve ser executado
+    void comoDesabilitarPorCondicional2() {
 
         Assertions.assertDoesNotThrow(() -> SaudacaoUtil.saudar(0));
     }
